95243630266881be724dec9bae3d712221218291
t-nishi421 <t.nishiyama2424@gmail.com>
2021-11-30 17:33:27 +0900

    教師なし学習追加　DL途中まで追加

--- 07_unsupervised_learning.py
+++ 07_unsupervised_learning.py
@@ -1,5 +1,10 @@
 # 教師なし学習
 ''' k-means法（k平均法）
+
+k-means法解説
+【機械学習】クラスタリングとは何か(k-means)
+<https://www.youtube.com/watch?v=8yptHd0JDlw>
+
 重心を更新してクラスタリングする方法
 （重心を更新しても移動しなくなったら収束）
 
@@ -43,6 +48,7 @@ c(1~m)
 - シルエット分析
 データの密集度合いのグラフからクラスタ数を決定する方法
 '''
+
 ''' DBSCAN
 ある点を中心とした円を考え、そこから到達可能な点群は同一クラスタとみなす手法
 密度準拠型といわれる
@@ -63,6 +69,9 @@ c(1~m)
 【PCA】
 Principal Components Analysis
 
+【次元圧縮】主成分解析（PCA）の理論
+<https://www.youtube.com/watch?v=mTmuwLF45T8>
+
 手順
 共分散行列を計算する
 Σ = 1/m Σ:n:i=1 (x(i))(x(i))T
@@ -73,8 +82,7 @@ U, S, V = np.linalg.svd(Sigma)
 
 ''' よく分らんやつ
 - 特異値分解（SVD）の数式
-- 半正定値エルミーと行列
-- カルバックライブラー情報量
+- 半正定値エルミーと行列  
 '''
 
 ''' 主成分分析の実装
@@ -84,6 +92,47 @@ Ureduce = U[:, :k]
 z = Ureduce.T@X
 '''
 
+''' カルバック・ライブラー情報量
+SNEの損失関数として利用されている
+<https://ai999.careers/rabbit/eshikaku/4-1-機械学習における損失関数とは？/>
+
+【SNE損失関数の計算式】
+カルバック・ライブラー情報量の和
+C = Σi KL(P||Q) = ΣiΣj≠i pj|i log pj|i/qj|i
+'''
+
+''' t-SNE
+【次元圧縮】t-SNE (t -distributed Stochastic Neighborhood Embedding)の理論
+<https://www.youtube.com/watch?v=hEanlhXGoME>
+
+【特徴】
+非線形で局所構造を保つのに優れている。
+（PCAは線形で対局構造を保つのに優れている）
+【批判】
+Preplexityが異なると異なるクラスターを形成することがある。
+まだまだ改善の余地が残っている。
+'''
+
+#####################################
+# k-means法の重心プロットテンプレート #
+#####################################
+import matplotlib as plt
+
+def plotData(X, idx=np.ones(X.shape[0],dtype=float)):
+    map = plt.get_cmap("rainbow")
+    idxn = idx.astype("float") / max(idx.astype("float"))
+    colors = map(idxn)
+    plt.scatter(X[:, 0], X[:, 1], 15, marker="o", c=colors, edgecolors=colors)
+    plt.draw()
+def plotProgress(X, centroids, idx=np.ones(X.shape[0],dtype=float)):
+    plt.hold(True)
+    plotData(X, idx)
+    plt.scatter(centroids[:, 0], centroids[:, 1], marker="x",s=200,linewidths=4, c="g")
+    plt.show()
+
+#################
+# k-means法実装 #
+#################
 import numpy as np
 
 def InitCentroids(X, K):
@@ -144,3 +193,104 @@ for i in range(1,6):
     idx = findClosestCentroids(X, centroids)
     plotProgress(X,centroids,idx)
 '''
+
+def runKmeans(X, K_init, epsilon, max_iter):
+    """ k-means法を繰り返す関数
+
+    Args:
+        X (np.ndarray): 訓練集合
+        K_init (int): 取得したいデータの個数
+        epsilon (float): ?
+        max_iter (int): 学習回数
+
+    Returns:
+        centroids (np.ndarray): 各クラスターの重心
+        idx (np.ndarray): 各データの属するクラスター番号
+    """
+    centroids_previous = InitCentroids(X, K_init)
+    idx = findClosestCentroids(X, centroids_previous)
+    centroids = computeClusterMeans(X, idx)
+    idx = findClosestCentroids(X, centroids)
+    num_iter = 0
+    while np.linalg.norm(centroids - centroids_previous, axis=1).mean() > epsilon and num_iter < max_iter:
+        centroids_previous = num_iter
+        centroids = computeClusterMeans(X, idx)
+        idx = findClosestCentroids(X, centroids)
+        num_iter += 1
+    return (centroids, idx)
+
+###########################
+# scikit-learnでk-means法 #
+###########################
+import sklearn.cluster as skc
+
+# インスタンス生成
+estimator = skc.KMeans(n_clusters=3, n_jobs=-1) #n_jobs=-1でCPUの全コア利用。デフォルトでは1コア利用。
+# 学習(k-means法)
+estimator.fit(X)
+# 学習結果をプロットしてみる
+idx = estimator.predict(X)
+centroids = estimator.cluster_centers_
+plotProgress(X,centroids,idx)
+
+#########################
+## 画像の色素数を落とす ##
+#########################
+A = scm.imread("{画像のパス}").astype(np.float32)/255
+#intで0から255で表現するのではなく、float64で0から1で表現することで、k平均法フィッティング時の型関係のトラブルを回避。
+#しかしメモリ節約にはならない。気になる人はもっとよい方法を考えよう。
+
+plt.axis("off")
+plt.imshow(A)
+img_size = A.shape
+print(img_size)
+Aflatten = A.reshape(img_size[0]*img_size[1], 3) # 2次元データに変換
+
+# クラスタリング
+K = 5 # 色素数
+estimator_pic = skc.KMeans(n_clusters=K, n_jobs=-1)
+estimator_pic.fit(Aflatten)
+color_idx = estimator_pic.predict(Aflatten)
+color_centroids = estimator_pic.cluster_centers_
+# 復元
+Aflatten_recovered = np.array([color_centroids[i] for i in color_idx])
+A_recovered = Aflatten_recovered.reshape(img_size)
+plt.axis("off")
+plt.imshow(A_recovered)
+
+###########
+# PCA実装 #
+###########
+def featureScaling(X):
+    """ 各特徴のレンジを揃える
+
+    Args:
+        X (np.ndarray): 訓練集合
+
+    Returns:
+        X_norm (np.ndarray): ノルム
+        mu (np.ndarray)    : 平均
+        std (np.ndarray)   : 標準偏差
+    """
+    mu = X.mean(axis=0)
+    std = X.std(axis=0)
+    X_norm = (X - mu) / std
+    return [X_norm, mu, std]
+
+def runPCA(X):
+    """ PCA
+    np.linagle.svdについて
+    https://numpy.org/doc/stable/reference/generated/numpy.linalg.svd.html  
+    https://qiita.com/kyoro1/items/4df11e933e737703d549
+
+    Args:
+        X (np.ndarray): 訓練集合
+
+    Returns:
+        U (np.ndarray): m*m のユニタリ行列
+        S (np.ndarray): m*n の実対角行列
+    """
+    m, _ = X.shape
+    Sigma = X.T@X / m
+    U, S, _ = np.linalg.svd(Sigma)
+    return [U, S]
---
+++ 08_DL.py
@@ -0,0 +1,25 @@
+# Deep Learning
+'''
+絶対に理解させる誤差逆伝播法【深層学習】
+https://www.youtube.com/watch?v=0itH0iDO8BE
+'''
+
+''' 勾配法
+【決定版】スーパーわかりやすい最適化アルゴリズム -損失関数からAdamとニュートン法-
+https://qiita.com/omiita/items/1735c1d048fe5f611f80
+
+- バッチ勾配法
+  どの繰り返しステップでも全ての訓練例を使う方法
+
+- 確率的勾配法降下法
+  各ステップでランダムにいくつかの訓練例を選んで使う勾配法
+
+- 共役勾配法
+  https://qiita.com/Dason08/items/27559e192a6a977dd5e5
+
+- BFGS法
+
+- Adam法
+
+などなど
+'''
