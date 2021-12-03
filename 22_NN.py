29158b9feb1eeca54638391306d9f981168b963c
t-nishi421 <t.nishiyama2424@gmail.com>
2021-12-03 17:56:39 +0900

    NN追加

--- 22_NN.py
+++ 22_NN.py
@@ -44,6 +44,12 @@ He初期化
 
 import numpy as np
 
+def sigmoid(x):
+    return 1/ (1+ np.exp(-x))
+
+def sigmoid_derivative(x):
+    return sigmoid(x) * (1-sigmoid(x))
+
 def cross_entropy_function(y_hat, y, m):
     """ クロスエントロピー関数
 
@@ -55,10 +61,10 @@ def cross_entropy_function(y_hat, y, m):
     Returns:
         [type]: cost
     """
-    return -np.sum(y * np.log(y_hat) + (1-y) * np.log(1-y_hat))
+    return -np.sum(y * np.log(y_hat) + (1-y) * np.log(1-y_hat)) / m
 
 def multi_class_cross_entropy_function(y_hat, y, m):
-    """ マルチクラスクロスエントロピー関数
+    """ マルチクラスのクロスエントロピー関数
 
     Args:
         y_hat ([type]): モデルの出力
@@ -81,11 +87,11 @@ def step(x):
 
 def relu(x):
     """ ReLu関数 """
-    return np.muximum(0, x)
+    return np.maximum(0, x)
 
-#######################
+######################
 # ミニバッチ勾配降下法 #
-#######################
+######################
 ''' sampleデータ
 X           = np.arrange(1, 16, 1)
 epochs      = 3
@@ -99,3 +105,49 @@ def mini_batch(X, epochs, sample_size, batch_size):
             X_batch = X[j:j + batch_size]
             print(X_batch)
         
+################
+# XOR回路の学習 #
+################
+cost = [] # 目的関数の出力値を保持する配列
+
+def XOR_model(W1, b1, W2, b2):
+    # データセットの準備
+    # XOR
+    X = np.array([[0,0], [0,1], [1,0], [1,1]])
+    y = np.array([[0], [1], [1], [0]])
+    m = X.shape[0]
+
+    # 学習率
+    learning_rate = 0.2  # 固定
+    # エポック 
+    epochs =  30000   # 固定
+    
+    for _ in range(epochs):
+
+        # フォワードプロパゲーション
+        # Layer1
+        layer1_z = X@W1 + b1
+        layer1_a = sigmoid(layer1_z)
+
+        #Layer2
+        layer2_z = layer1_a@W2 + b2
+        layer2_a = sigmoid(layer2_z)
+        
+        # コスト関数
+        cost.append(- np.sum(y * np.log(layer2_a) + (1-y) * np.log(1 - layer2_a)) / m )
+        
+        # バックプロパゲーション
+        derivative_layer2_z = (layer2_a - y) / m
+        delta_W2 = layer1_a.T@derivative_layer2_z
+        delta_b2 = np.sum(derivative_layer2_z, axis=0, keepdims=True)
+    
+        derivative_layer1_z = derivative_layer2_z@W2.T * sigmoid_derivative(layer1_z)
+        delta_W1 = X.T@derivative_layer1_z
+        delta_b1 = np.sum(derivative_layer1_z, axis=0, keepdims=True)
+    
+        # パラメータの更新
+        W2 = W2 - learning_rate * delta_W2
+        b2 = b2 - learning_rate * delta_b2
+        W1 = W1 - learning_rate * delta_W1
+        b1 = b1 - learning_rate * delta_b1
+    return layer2_a, W1, b1, W2, b2
